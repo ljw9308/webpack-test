fc983853c01ab9537c85274730caf3635cf3901b
MW <MW>
2017-08-14 21:35:46 +0800

    dd

--- app/main.js
+++ app/main.js
@@ -1,3 +1,22 @@
 //main.js 
 var greeter = require('./green.js');
+import _ from 'lodash';    //lodash 是一个前端的工具库  
+import '../src/style.css';    //引入一个css样式
+import Ico from './../src/dianchi.png';
+
+//引入样式
+function compoent(){
+   var div = document.createElement('div');
+   div.id = 'compoent';
+   div.innerHTML = _.join(['hellow','webpack'], ' ');
+   
+// var img = new Image();
+// img.classList.add('ico');
+// img.src = Ico;
+// div.appendChild(img);
+   return div;
+}
+
 document.getElementById('root').appendChild(greeter());
+
+document.body.appendChild(compoent())
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,23 @@ const config = {
 		app: __dirname + '/app/main.js'
         /*vendors: __dirname + '/app/vendors.js'*/
 	},
+	module: {
+		rules:[
+		  {
+		    test: /\.css$/,
+		    use: [
+		       'style-loader',
+		       'css-loader'
+		    ]
+		  },
+		  {
+	         test: /\.(png|svg|jpg|gif)$/,
+	         use: [
+	           'file-loader'
+	         ]
+	       }
+		]
+	},
 	output: {
 		filename: 'bundle.js',
 		path: __dirname + '/assets'
